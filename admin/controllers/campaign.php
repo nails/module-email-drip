ab495680c090e672bee0ff88970dd6fcdfb2e6a4
Pablo de la Peña <p@shedcollective.org>
2016-01-08 14:54:10 +0000

    Knockout is loaded automatically by admin

--- admin/controllers/campaign.php
+++ admin/controllers/campaign.php
@@ -181,7 +181,6 @@ class Campaign extends BaseAdmin
 
         // --------------------------------------------------------------------------
 
-        $this->asset->library('KNOCKOUT');
         $this->asset->load('nails.admin.emaildrip.campaign.min.js', 'NAILS');
         $this->asset->inline(
             'ko.applyBindings(new dripCampaignEdit([]));',
@@ -256,7 +255,6 @@ class Campaign extends BaseAdmin
 
         // --------------------------------------------------------------------------
 
-        $this->asset->library('KNOCKOUT');
         $this->asset->load('nails.admin.emaildrip.campaign.min.js', 'NAILS');
         $this->asset->inline(
             'ko.applyBindings(new dripCampaignEdit(' . json_encode($this->data['campaign']->emails) . '));',
