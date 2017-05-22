570f170272e21c5b574664793b798fe1361506f2
Pablo de la Peña <p@shedcollective.org>
2017-05-22 23:47:31 +0100

    Using Factory loaded services instead of magic property

--- src/Controller/BaseAdmin.php
+++ src/Controller/BaseAdmin.php
@@ -13,12 +13,14 @@
 namespace Nails\EmailDrip\Controller;
 
 use Nails\Admin\Controller\Base;
+use Nails\Factory;
 
 class BaseAdmin extends Base
 {
     public function __construct()
     {
         parent::__construct();
-        $this->asset->load('admin.css', 'nailsapp/module-email-drip');
+        $oAsset = Factory::service('Asset');
+        $oAsset->load('admin.css', 'nailsapp/module-email-drip');
     }
 }
