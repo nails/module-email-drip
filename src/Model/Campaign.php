f9d3bfb2e13468587d449a6fbd70626983a8cabd
Pablo de la Peña <p@shedcollective.org>
2018-07-19 18:25:05 +0100

    Added typehints for models

--- src/Model/Campaign.php
+++ src/Model/Campaign.php
@@ -36,7 +36,7 @@ class Campaign extends Base
      * @param bool|false $bIncludeDeleted Whether to include deleted results
      * @return array
      */
-    public function getAll($iPage = null, $iPerPage = null, $aData = array(), $bIncludeDeleted = false)
+    public function getAll($iPage = null, $iPerPage = null, array $aData = array(), $bIncludeDeleted = false)
     {
         //  If the first value is an array then treat as if called with getAll(null, null, $aData);
         //  @todo (Pablo - 2017-11-09) - Convert these to expandable fields
--- src/Model/CampaignEmail.php
+++ src/Model/CampaignEmail.php
@@ -31,10 +31,10 @@ class CampaignEmail extends Base
 
     protected function formatObject(
         &$oObj,
-        $aData = array(),
-        $aIntegers = array(),
-        $aBools = array(),
-        $aFloats = array()
+        array $aData = [],
+        array $aIntegers = [],
+        array $aBools = [],
+        array $aFloats = []
     ) {
         parent::formatObject($oObj, $aData, $aIntegers, $aBools, $aFloats);
 
