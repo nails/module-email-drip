c35d8f3437be38e4f9988026c0b6067b94dbea02
Pablo de la Peña <p@shedcollective.org>
2019-02-27 19:45:35 +0000

    fix: Added ensuring method signature compatability

--- admin/controllers/Campaign.php
+++ admin/controllers/Campaign.php
@@ -40,7 +40,7 @@ class Campaign extends BaseAdmin
      * Returns an array of extra permissions for this controller
      * @return array
      */
-    public static function permissions()
+    public static function permissions(): array
     {
         $permissions = parent::permissions();
 
