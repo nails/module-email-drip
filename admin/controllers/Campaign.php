11fe5251436cb7c985d1cdcb73ce40d849a7628d
Pablo de la Peña <p@shedcollective.org>
2019-09-12 16:44:41 +0100

    Using module constants rather than hardcoded slugs

--- admin/controllers/Campaign.php
+++ admin/controllers/Campaign.php
@@ -12,14 +12,16 @@
 
 namespace Nails\Admin\EmailDrip;
 
-use Nails\Factory;
+use Nails\Auth;
 use Nails\Admin\Helper;
 use Nails\EmailDrip\Controller\BaseAdmin;
+use Nails\Factory;
 
 class Campaign extends BaseAdmin
 {
     /**
      * Announces this controller's navGroups
+     *
      * @return stdClass
      */
     public static function announce()
@@ -38,6 +40,7 @@ class Campaign extends BaseAdmin
 
     /**
      * Returns an array of extra permissions for this controller
+     *
      * @return array
      */
     public static function permissions(): array
@@ -56,6 +59,7 @@ class Campaign extends BaseAdmin
 
     /**
      * Browse drip campaigns
+     *
      * @return void
      */
     public function index()
@@ -78,30 +82,30 @@ class Campaign extends BaseAdmin
         // --------------------------------------------------------------------------
 
         //  Get pagination and search/sort variables
-        $page      = $oInput->get('page')      ? $oInput->get('page')      : 0;
-        $perPage   = $oInput->get('perPage')   ? $oInput->get('perPage')   : 50;
-        $sortOn    = $oInput->get('sortOn')    ? $oInput->get('sortOn')    : $sTableAlias . '.label';
+        $page      = $oInput->get('page') ? $oInput->get('page') : 0;
+        $perPage   = $oInput->get('perPage') ? $oInput->get('perPage') : 50;
+        $sortOn    = $oInput->get('sortOn') ? $oInput->get('sortOn') : $sTableAlias . '.label';
         $sortOrder = $oInput->get('sortOrder') ? $oInput->get('sortOrder') : 'desc';
-        $keywords  = $oInput->get('keywords')  ? $oInput->get('keywords')  : '';
+        $keywords  = $oInput->get('keywords') ? $oInput->get('keywords') : '';
 
         // --------------------------------------------------------------------------
 
         //  Define the sortable columns
-        $sortColumns = array(
+        $sortColumns = [
             $sTableAlias . '.created'  => 'Created Date',
             $sTableAlias . '.modified' => 'Modified Date',
-            $sTableAlias . '.label'    => 'label'
-        );
+            $sTableAlias . '.label'    => 'label',
+        ];
 
         // --------------------------------------------------------------------------
 
         //  Define the $data variable for the queries
-        $data = array(
-            'sort' => array(
-                array($sortOn, $sortOrder)
-            ),
-            'keywords' => $keywords
-        );
+        $data = [
+            'sort'     => [
+                [$sortOn, $sortOrder],
+            ],
+            'keywords' => $keywords,
+        ];
 
         //  Get the items for the page
         $totalRows               = $oCampaignModel->countAll($data);
@@ -128,6 +132,7 @@ class Campaign extends BaseAdmin
 
     /**
      * Create a new drip campaign
+     *
      * @return void
      */
     public function create()
@@ -153,7 +158,7 @@ class Campaign extends BaseAdmin
 
                 if ($oCampaignModel->create($this->getPostObject())) {
 
-                    $oSession = Factory::service('Session', 'nails/module-auth');
+                    $oSession = Factory::service('Session', Auth\Constants::MODULE_SLUG);
                     $oSession->setFlashData('success', 'Successfully created drip campaign.');
                     redirect('admin/emaildrip/campaign/index');
 
@@ -178,6 +183,7 @@ class Campaign extends BaseAdmin
 
     /**
      * Edit a drip campaign
+     *
      * @return void
      */
     public function edit()
@@ -212,7 +218,7 @@ class Campaign extends BaseAdmin
 
                 if ($oCampaignModel->update($this->data['campaign']->id, $this->getPostObject())) {
 
-                    $oSession = Factory::service('Session', 'nails/module-auth');
+                    $oSession = Factory::service('Session', Auth\Constants::MODULE_SLUG);
                     $oSession->setFlashData('success', 'Successfully updated drip campaign.');
                     redirect('admin/emaildrip/campaign/index');
 
@@ -245,7 +251,7 @@ class Campaign extends BaseAdmin
         $this->data['segments'] = $oSegmentModel->getAllFlat();
 
         //  Load assets
-        $aEmails = $oItem ? $oItem->email->data : array();
+        $aEmails = $oItem ? $oItem->email->data : [];
 
         $oAsset->load('admin.campaign.edit.min.js', 'nails/module-email-drip');
         $oAsset->inline(
@@ -258,6 +264,7 @@ class Campaign extends BaseAdmin
 
     /**
      * Runs form validation
+     *
      * @return boolean
      */
     protected function formValidation()
@@ -283,6 +290,7 @@ class Campaign extends BaseAdmin
 
     /**
      * Delete a drip campaign
+     *
      * @return void
      */
     public function delete()
@@ -316,7 +324,7 @@ class Campaign extends BaseAdmin
 
         // --------------------------------------------------------------------------
 
-        $oSession = Factory::service('Session', 'nails/module-auth');
+        $oSession = Factory::service('Session', Auth\Constants::MODULE_SLUG);
         $oSession->setFlashData($sStatus, $sMessage);
 
         redirect('admin/emaildrip/campaign/index');
