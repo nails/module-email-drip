1f3a487dfac3cc1b0df4f7b0fcb0153ab5f6fc98
Pablo de la Peña <p@shedcollective.org>
2020-11-09 09:53:46 +0000

    Removes deprecated webpack option

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: '../css/[name].min.css',
-            allChunks: true
+            filename: '../css/[name].min.css'
         })
     ],
     mode: 'production'
